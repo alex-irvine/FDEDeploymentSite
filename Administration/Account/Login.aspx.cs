328714c3f777b7bd0d92ae51919b92f7204a6072
RandomTony <anthony.lohou@laposte.net>
2016-04-20 13:02:25 +0100

    logon Form and register Form

--- Administration/Account/Login.aspx.cs
+++ Administration/Account/Login.aspx.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using Administration.Models;
+using System.Web.UI.HtmlControls.HtmlControl;
 
 namespace Administration.Account
 {
@@ -33,29 +34,46 @@ namespace Administration.Account
                 
                 // This doen't count login failures towards account lockout
                 // To enable password failures to trigger lockout, change to shouldLockout: true
-                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
-                using(ServiceReference1.ServiceClient = new ServiceReference1.ServiceClient()){
-                    Person pson = ClientID.GetPerson("");
-                }
-                switch (result)
+                //var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+                using (ServiceReference1.Service1Client client = new ServiceReference1.Service1Client())
                 {
-                    case SignInStatus.Success:
-                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                        break;
-                    case SignInStatus.LockedOut:
-                        Response.Redirect("/Account/Lockout");
-                        break;
-                    case SignInStatus.RequiresVerification:
-                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
-                                                        Request.QueryString["ReturnUrl"],
-                                                        RememberMe.Checked),
-                                          true);
-                        break;
-                    case SignInStatus.Failure:
-                    default:
-                        FailureText.Text = "Invalid login attempt";
-                        ErrorMessage.Visible = true;
-                        break;
+                    
+                    ServiceReference1.Person pson = client.GetPerson(Email.Text/*,Password.Text*/);
+                    if (pson!= null)
+                    {
+                        if (pson.IsAdmin == true) {
+                            // Log
+                        }
+                        else
+                        {
+                            // Error, you can't access
+                            FailureText.Text = "You don't have permission to access";
+                            ErrorMessage.Visible = true;
+                            // Redirect ?
+                        }
+                        //IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                    }
+                    /*
+                    switch (result)
+                    {
+                        case SignInStatus.Success:
+                            IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                            break;
+                        case SignInStatus.LockedOut:
+                            Response.Redirect("/Account/Lockout");
+                            break;
+                        case SignInStatus.RequiresVerification:
+                            Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}",
+                                                            Request.QueryString["ReturnUrl"],
+                                                            RememberMe.Checked),
+                                              true);
+                            break;
+                        case SignInStatus.Failure:
+                        default:
+                            FailureText.Text = "Invalid login attempt";
+                            ErrorMessage.Visible = true;
+                            break;
+                    }*/
                 }
             }
         }
--- Administration/Account/Register.aspx.cs
+++ Administration/Account/Register.aspx.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using Administration.Models;
+using System.Text.RegularExpressions;
 
 namespace Administration.Account
 {
@@ -15,22 +16,42 @@ namespace Administration.Account
         {
             var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
             var signInManager = Context.GetOwinContext().Get<ApplicationSignInManager>();
-            var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
-            IdentityResult result = manager.Create(user, Password.Text);
-            if (result.Succeeded)
+            //var user = new ApplicationUser() { UserName = Email.Text, Email = Email.Text };
+            //IdentityResult result = manager.Create(user, Password.Text);
+            /*if (result.Succeeded)
             {
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 //string code = manager.GenerateEmailConfirmationToken(user.Id);
                 //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
                 //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
                 
-                signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
-                IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                //signInManager.SignIn( user, isPersistent: false, rememberBrowser: false);
+                //IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
             }
             else 
             {
                 ErrorMessage.Text = result.Errors.FirstOrDefault();
+            }*/
+            if (checkMail(Email.Text))
+            {
+                using (ServiceReference1.Service1Client client = new ServiceReference1.Service1Client())
+                {
+                    //Check mail is not already used
+
+                    //Check password and Hash
+
+                    // Add to database
+                }
             }
+            else
+            {
+                ErrorMessage.Text = "Wrong email address format";
+            }
+        }
+
+        private bool checkMail(string mail) {
+            Regex rgx = new Regex("^[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)*(.[a-zA-Z]{2,3})+$");
+            return rgx.IsMatch(mail);
         }
 
         protected void Email_TextChanged(object sender, EventArgs e)
